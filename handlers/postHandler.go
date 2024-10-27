64d7cb702e4fb22cb4c3ae02de4b0473d60f48a1
tvallois <theo.vallois@hotmail.fr>
2024-10-27 15:11:47 +0100

    added post and user handlers

--- handlers/postHandler.go
+++ handlers/postHandler.go
@@ -1,1 +1,58 @@
 package handlers
+
+import (
+	"Forum/database"
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+func CreatePost(w http.ResponseWriter, r *http.Request) {
+	//Set response header to JSON
+	w.Header().Set("Content-Type", "application/json")
+
+	//Check request method in POST
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	//Decode request body intot a Post struct
+	var post database.Post
+	if err := json.NewDecoder(r.Body).Decode(&post); err != nil {
+		http.Error(w, "Invalid request playload", http.StatusBadRequest)
+		return
+	}
+
+	//Post validation options
+	if post.Title == "" || post.Content == "" {
+		http.Error(w, "Title and Content are require", http.StatusBadRequest)
+		return
+	}
+
+	//set creationDate + default values
+	post.CreationDate = time.Now()
+	post.UpdateDate = post.CreationDate
+	post.IsDeleted = false
+
+	//call function to save the post in database
+	if err := database.CreatePost(&post); err != nil {
+		http.Error(w, "Failed to create post", http.StatusInternalServerError)
+		return
+	}
+
+	//Respond with created post
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(post)
+}
+
+func GetPost(w http.ResponseWriter, r *http.Request) {
+	posts, err := database.GetAllPosts()
+	if err != nil {
+		http.Error(w, "Failed to fetch posts", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(posts)
+}
--- handlers/userHandler.go
+++ handlers/userHandler.go
@@ -1,1 +1,77 @@
 package handlers
+
+import (
+	"Forum/database"
+	"encoding/json"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// Hash password using bcrypt & save User to database
+func RegisterUser(w http.ResponseWriter, r *http.Request) {
+	var client database.Client
+	if err := json.NewDecoder(r.Body).Decode(&client); err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(client.Password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, "Could not hash password", http.StatusInternalServerError)
+		return
+	}
+	client.Password = string(hashedPassword)
+
+	if err := database.CreateClient(&client); err != nil {
+		http.Error(w, "Could not create user", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(client)
+}
+
+// Verify user's credentials and create session
+func LoginUser(w http.ResponseWriter, r *http.Request) {
+	var client database.Client
+	if err := json.NewDecoder(r.Body).Decode(&client); err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+
+	//Retrieve stored client data from the provided email address
+	storedClient, err := database.GetClientByEmail(client.Email)
+	if err != nil || storedClient == nil {
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	//Compare provided password with stored hashed password
+	if err := bcrypt.CompareHashAndPassword([]byte(storedClient.Password), []byte(client.Password)); err != nil {
+		http.Error(w, "Invalid Credentials", http.StatusUnauthorized)
+		return
+	}
+
+	//Create session for user
+	sessionID, err := database.CreateUserSession(storedClient.UserID)
+	if err != nil {
+		http.Error(w, "Could not create session", http.StatusInternalServerError)
+		return
+	}
+
+	//Respond with sessionID + wanted details
+	response := struct {
+		SessionID string `json:"session_id"`
+		UserID    int    `json:"user_id"`
+		UserName  string `json:"user_name"`
+	}{
+		SessionID: sessionID,
+		UserID:    storedClient.UserID,
+		UserName:  storedClient.UserName,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
