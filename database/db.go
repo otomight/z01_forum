0748c62af1b30fb5597311ae5fa9d5d80da8444f
tvallois <theo.vallois@hotmail.fr>
2024-10-27 11:02:51 +0100

    initializing db connection

--- database/db.go
+++ database/db.go
@@ -1,1 +1,26 @@
 package database
+
+import (
+	"database/sql"
+	"log"
+)
+
+var DB *sql.DB
+
+func InitDB() (*sql.DB, error) {
+	//Open DB connection
+	var err error
+	DB, err = sql.Open("sqlite3", "./forum.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Enable foreign key for SQLite
+	_, err = DB.Exec("PRAGMA foreign_key = ON")
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Database initialized successfully")
+	return DB, nil
+}
