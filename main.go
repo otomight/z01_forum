fb77bd16f296c924dba8babd3233e483929b46b4
tvallois <theo.vallois@hotmail.fr>
2024-10-27 16:11:42 +0100

    main func to test the server

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"Forum/server"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := server.InitializeServer()
+
+	log.Println("Starting server on : 8081")
+	if err := http.ListenAndServe(":8081", mux); err != nil {
+		log.Fatalf("Could not start server: %v", err)
+	}
+}
