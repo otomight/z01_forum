2cd3e07ed10497cbefbd2b689800f5136d5d2cdd
tvallois <theo.vallois@hotmail.fr>
2024-10-27 16:10:11 +0100

    set the server initialization

--- server/server.go
+++ server/server.go
@@ -1,1 +1,22 @@
 package server
+
+import (
+	"Forum/handlers"
+	"Forum/middleware"
+	"net/http"
+)
+
+func InitializeServer() http.Handler {
+	mux := http.NewServeMux()
+
+	//Routes
+	mux.HandleFunc("/posts", handlers.GetPost)
+
+	//Protect route to create new Post with authentication
+	mux.Handle("psots/create", middleware.LoggingMiddleware(http.HandlerFunc(handlers.CreatePost)))
+
+	//Wrap mux with logging middleware
+	wrappedMux := middleware.LoggingMiddleware(mux)
+
+	return wrappedMux
+}
