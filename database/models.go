c3791108a057813702e3a319fa03db08713fe772
tvallois <theo.vallois@hotmail.fr>
2024-10-27 11:38:11 +0100

    implemented client CRUD operations

--- database/models.go
+++ database/models.go
@@ -37,3 +37,22 @@ type Post struct {
 	DeletionDate time.Time `json:"deletion_date"`
 	IsDeleted    bool      `json:"is_deleted"`
 }
+
+func CreateClient(client *Client) error {
+	_, err := DB.Exec(`
+		INSERT INTO clients (last_name, first_name, user_name, email, password, avatar, bith_date)
+		VALUES 	(?, ?, ?, ?, ?, ?, ?)
+	`, client.LastName, client.FirstName, client.UserName, client.Email, client.Password, client.Avatar, client.BirthDate)
+	return err
+}
+
+func GetClientByID(userID int) (*Client, error) {
+	row := DB.QueryRow(`
+		SELECT user_id, last_name, first_name, user_name, email, avatar, birth_date, creation_date, update_date, deletion_date
+		FROM clients WERE user_id = ?
+	`, userID)
+	var client Client
+	err := row.Scan(&client.UserID, &client.LastName, &client.FirstName, &client.UserName, &client.Email,
+		&client.BirthDate, &client.CreationDate, &client.UpdateDate, &client.DeletionDate)
+	return &client, err
+}
