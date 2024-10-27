2d9bd976ebc58540ee2e4be7d5a128f5162dd517
tvallois <theo.vallois@hotmail.fr>
2024-10-27 11:24:50 +0100

    added execution of the create_table shema script

--- database/db.go
+++ database/db.go
@@ -21,6 +21,53 @@ func InitDB() (*sql.DB, error) {
 		return nil, err
 	}
 
+	//Execute create_table schema script
+	if _, err := DB.Exec(schema); err != nil {
+		log.Fatal("Error executing schema:", err)
+	}
 	log.Println("Database initialized successfully")
 	return DB, nil
 }
+
+const schema = `
+-- sessions table schema
+CREATE TABLE IF NOT EXISTS Sessions (
+    SessionID TEXT PRIMARY KEY,
+    userID INTEGER,
+    expiration TIMESTAMP,
+    creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updateDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deletionDate TIMESTAMP,
+    isDeleted BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY(user_id) REFERENCES clients(user_id)
+);
+
+-- posts table schema
+CREATE TABLE IF NOT EXISTS Posts (
+    postID INTEGER PRIMARY KEY AUTOINCREMENT,
+    authorID INTEGER,
+    Title TEXT,
+    category TEXT,
+    content TEXT,
+    creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updateDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    DeletionDate TIMESTAMP,
+    isDeleted BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY(author_id) REFERENCES clients(user_id)
+);
+
+-- clients table schema
+CREATE TABLE IF NOT EXISTS Clients (
+    userID INTEGER PRIMARY KEY AUTOINCREMENT,
+    lastName TEXT,
+    firstName TEXT,
+    userName TEXT UNIQUE,
+    email TEXT UNIQUE,
+    password TEXT,
+    avatar TEXT,
+    birthDate DATE,
+    creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updateDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deletionDate TIMESTAMP 
+);
+`
