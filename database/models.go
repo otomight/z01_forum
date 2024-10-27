5cdc5927c5916f997a9e7eba01621dea7d82a366
tvallois <theo.vallois@hotmail.fr>
2024-10-27 11:53:30 +0100

    implemented session CRUD operations

--- database/models.go
+++ database/models.go
@@ -38,9 +38,10 @@ type Post struct {
 	IsDeleted    bool      `json:"is_deleted"`
 }
 
+// Client CRUD operations
 func CreateClient(client *Client) error {
 	_, err := DB.Exec(`
-		INSERT INTO clients (last_name, first_name, user_name, email, password, avatar, bith_date)
+		INSERT INTO Clients (last_name, first_name, user_name, email, password, avatar, bith_date)
 		VALUES 	(?, ?, ?, ?, ?, ?, ?)
 	`, client.LastName, client.FirstName, client.UserName, client.Email, client.Password, client.Avatar, client.BirthDate)
 	return err
@@ -49,10 +50,30 @@ func CreateClient(client *Client) error {
 func GetClientByID(userID int) (*Client, error) {
 	row := DB.QueryRow(`
 		SELECT user_id, last_name, first_name, user_name, email, avatar, birth_date, creation_date, update_date, deletion_date
-		FROM clients WERE user_id = ?
+		FROM Clients WERE user_id = ?
 	`, userID)
 	var client Client
 	err := row.Scan(&client.UserID, &client.LastName, &client.FirstName, &client.UserName, &client.Email,
 		&client.BirthDate, &client.CreationDate, &client.UpdateDate, &client.DeletionDate)
 	return &client, err
+}
+
+// Session CRUD operations
+func CreateSession(session *Session) error {
+	_, err := DB.Exec(`
+		INSERT INTO Sessions (session_id, user_id, expiration)
+		VALUES (?, ?, ?)
+	`, session.SessionID, session.UserID, session.Expiration)
+	return err
+}
+
+func GetSession(sessionID string) (*Session, error) {
+	row := DB.QueryRow(`
+		SELECT session_id, user_id, expiration, creation_date, update_date, deletion_date, is_deleted
+		FROM Sessions WHERE session_id = ?
+	`, sessionID)
+	var session Session
+	err := row.Scan(&session.SessionID, &session.UserID, &session.Expiration, &session.CreationDate,
+		&session.UpdateDate, &session.DeletionDate, &session.IsDeleted)
+	return &session, err
 }
