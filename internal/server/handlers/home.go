5629bd3d48f4f51b845db65a3dca19063375873d
Adrien DEBADIER <adriendebadier@hotmail.com>
2024-11-16 16:09:25 +0100

    remove prints, fix render home template

--- internal/server/handlers/home.go
+++ internal/server/handlers/home.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"forum/internal/config"
 	"forum/internal/database"
 	"forum/internal/server/models"
@@ -14,11 +13,9 @@ func HomePageHandler(w http.ResponseWriter, r *http.Request) {
 	var session	*database.UserSession
 	var posts	[]database.Post
 
-	session, err := services.GetSession(r)
+	session, _ = services.GetSession(r)
 	posts, _ = database.GetAllPosts()
 	// Prepare posts (assuming posts are public and do not depend on login)
-	fmt.Println("ERROR:", err)
-	fmt.Println(session.IsLoggedIn)
 	data := models.HomePageData{
 		Posts:      posts,
 		Session:	session,
