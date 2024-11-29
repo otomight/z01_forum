942733dfa747422d38b5eef6352dbc9b18773606
Adrien DEBADIER <adriendebadier@hotmail.com>
2024-11-29 11:32:50 +0100

    Pretty .sql UwU

--- internal/database/create_table.sql
+++ internal/database/create_table.sql
@@ -1,79 +1,79 @@
 CREATE TABLE IF NOT EXISTS clients (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	last_name TEXT,
-	first_name TEXT,
-	user_name TEXT UNIQUE,
-	email TEXT UNIQUE,
-	oauth_provider TEXT,
-	oauth_id TEXT UNIQUE,
-	password TEXT,
-	avatar TEXT,
-	birth_date DATE,
-	user_role TEXT DEFAULT 'user',
-	creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	deletion_date TIMESTAMP
+	id				INTEGER PRIMARY KEY AUTOINCREMENT,
+	last_name		TEXT,
+	first_name		TEXT,
+	user_name		TEXT UNIQUE,
+	email			TEXT UNIQUE,
+	oauth_provider	TEXT,
+	oauth_id		TEXT UNIQUE,
+	password 		TEXT,
+	avatar			TEXT,
+	birth_date		DATE,
+	user_role		TEXT DEFAULT 'user',
+	creation_date	TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	update_date		TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	deletion_date	TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS sessions (
-	id TEXT PRIMARY KEY,
-	user_id INTEGER,
-	user_role TEXT,
-	user_name TEXT,
-	expiration TIMESTAMP,
-	creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	deletion_date TIMESTAMP,
-	is_deleted BOOLEAN DEFAULT FALSE,
+	id				TEXT PRIMARY KEY,
+	user_id			INTEGER NOT NULL,
+	user_role		TEXT,
+	user_name		TEXT,
+	expiration		TIMESTAMP,
+	creation_date	TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	update_date		TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	deletion_date	TIMESTAMP,
+	is_deleted		BOOLEAN DEFAULT FALSE,
 	FOREIGN KEY(user_id) REFERENCES clients(id)
 );
 
 CREATE TABLE IF NOT EXISTS categories (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	name TEXT
+	id		INTEGER PRIMARY KEY AUTOINCREMENT,
+	name	TEXT
 );
 
 CREATE TABLE IF NOT EXISTS posts (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	author_id INTEGER,
-	title TEXT,
-	category TEXT,
-	tags TEXT,
-	content TEXT,
-	creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	deletion_date TIMESTAMP,
-	is_deleted BOOLEAN DEFAULT FALSE,
-	likes INTEGER DEFAULT 0,
-	dislikes INTEGER DEFAULT 0,
+	id				INTEGER PRIMARY KEY AUTOINCREMENT,
+	author_id		INTEGER NOT NULL,
+	title			TEXT,
+	category		TEXT,
+	tags			TEXT,
+	content			TEXT,
+	creation_date	TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	update_date		TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	deletion_date	TIMESTAMP,
+	is_deleted		BOOLEAN DEFAULT FALSE,
+	likes			INTEGER DEFAULT 0,
+	dislikes		INTEGER DEFAULT 0,
 	FOREIGN KEY(author_id) REFERENCES clients(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS posts_categories (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	category_id INTEGER,
-	post_id INTEGER,
+	id				INTEGER PRIMARY KEY AUTOINCREMENT,
+	category_id		INTEGER,
+	post_id			INTEGER,
 	FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
 	FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
 	UNIQUE(category_id, post_id)
 );
 
 CREATE TABLE IF NOT EXISTS comments (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	post_id INT NOT NULL,
-	user_id INT NOT NULL,
-	content TEXT,
-	creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	id				INTEGER PRIMARY KEY AUTOINCREMENT,
+	post_id			INTEGER NOT NULL,
+	user_id			INTEGER NOT NULL,
+	content			TEXT,
+	creation_date	TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
 	FOREIGN KEY (user_id) REFERENCES clients(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS likes_dislikes (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	post_id INT NOT NULL,
-	user_id INT NOT NULL,
-	liked BOOLEAN DEFAULT NULL,
-	update_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	id			INTEGER PRIMARY KEY AUTOINCREMENT,
+	post_id		INTEGER NOT NULL,
+	user_id		INTEGER NOT NULL,
+	liked		BOOLEAN DEFAULT NULL,
+	update_date	TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
 	FOREIGN KEY (user_id) REFERENCES clients(id) ON DELETE CASCADE,
 	UNIQUE (post_id, user_id)
