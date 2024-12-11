ac423925c9fedd431b731d362004e84c651b40fc
Adrien DEBADIER <adriendebadier@hotmail.com>
2024-12-11 15:32:26 +0100

    Remove category page template.

--- internal/server/models/pagedatas.go
+++ internal/server/models/pagedatas.go
@@ -3,41 +3,41 @@ package models
 import db "forum/internal/database"
 
 type ViewPostPageData struct {
-	Session		*db.UserSession
+	Session		*db.UserSession // | null
 	Categories	[]*db.Category
 	Post		*db.Post
 }
 
 type LoginPageData struct {
-	Session		*db.UserSession
+	Session		*db.UserSession // | null
 	Categories	[]*db.Category
 }
 
 type RegisterPageData struct {
-	Session		*db.UserSession
+	Session		*db.UserSession // | null
 	Categories	[]*db.Category
 }
 
 type CreatePostPageData struct {
-	Session		*db.UserSession
+	Session		*db.UserSession // | null
 	Categories	[]*db.Category
 }
 
 type HistoryPageData struct {
-	Session		*db.UserSession
+	Session		*db.UserSession // | null
 	Categories	[]*db.Category
 	Posts		[]*db.Post
 }
 
 type CategoryPostsPageData struct {
-	Session		*db.UserSession
+	Session		*db.UserSession // | null
 	Categories	[]*db.Category
 	Category	*db.Category
 	Posts		[]*db.Post
 }
 
 type HomePageData struct {
-	Session		*db.UserSession
+	Session		*db.UserSession // | null
 	Categories	[]*db.Category
 	Posts		[]*db.Post
 }
